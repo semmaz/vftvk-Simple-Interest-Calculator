ed6f7da87d8b052121bffd8e133f6c88bdb42684
Simon Mazur <semmaz.box@gmail.com>
2021-11-18 15:28:36 +0200

    Fixed The calculator UI and logic

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(ev) {
+    ev.preventDefault();
+    const { principal, rate, years, result } = this.elements;
+    if (principal.value <= 0) {
+        alert("Enter a positive number");
+        // focus input element
+        document.getElementById("principal").focus();
+        return;
+    }
+    const interest = +principal.value * +years.value * +rate.value / 100;
+    const year = new Date().getFullYear() + parseInt(years.value);
+
+    result.innerHTML = `
+        <br>
+        If you deposit <mark>${principal.value}</mark>,<br>
+        at an interest rate of <mark>${rate.value}%</mark>.<br>
+        You will receive an amount of <mark>${interest}</mark>,<br>
+        in the year <mark>${year}</mark>
+    `;
 }
-        
+
+function updateRate() {
+    const rateval = this.value;
+    document.getElementById("rate_val").innerText = `${rateval}%`;
+}
+
+document.getElementById('sic').addEventListener("submit", compute);
+document.getElementById('rate').addEventListener("change", updateRate);
